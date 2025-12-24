8a8edff69a2fa34215d1222380ec35ac074c15c3
Nguyen Quang Huong <nguyenquanghuong_t67@hus.edu.vn>
2025-12-24 15:54:43 +0700

    Resolve conflicts and add History Borrow Device feature

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -2,21 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import TeacherRegister from '../pages/TeacherRegister.vue'
 import BorrowDevice from '../pages/BorrowDevice.vue'
-<<<<<<< Updated upstream
-=======
 import DeviceRegister from '../pages/DeviceRegister.vue'
 import HistoryBorrowDevice from '../pages/HistoryBorrowDevice.vue'
->>>>>>> Stashed changes
+
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/teachers/register', name: 'teacher-register', component: TeacherRegister },
   { path: '/transactions/borrow', name: 'borrow-device', component: BorrowDevice },
-<<<<<<< Updated upstream
-=======
   { path: '/devices/register', name: 'device-register', component: DeviceRegister },
   { path: '/history/borrow_device', name: 'transaction-list', component: HistoryBorrowDevice },
->>>>>>> Stashed changes
 ]
 
 const router = createRouter({
