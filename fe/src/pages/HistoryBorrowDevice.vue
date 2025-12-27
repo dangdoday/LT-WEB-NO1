a8a4ee4e3e068233a12207b4070bbec2c883db78
Nguyen Quang Huong <nguyenquanghuong_t67@hus.edu.vn>
2025-12-27 18:45:01 +0700

    cải thiện UI ô input tìm kiếm Thiết bị

--- fe/src/pages/HistoryBorrowDevice.vue
+++ fe/src/pages/HistoryBorrowDevice.vue
@@ -73,7 +73,9 @@ onMounted(() => {
           <label>Giáo viên</label>
           <select v-model="filters.teacher_id">
             <option value="">Chọn giáo viên</option>
-            <option v-for="t in options.teachers" :key="t.id" :value="t.id">{{ t.name }}</option>
+            <option v-for="t in options.teachers" :key="t.id" :value="t.id">
+              {{ t.name }}
+            </option>
           </select>
         </div>
 
@@ -188,13 +190,15 @@ onMounted(() => {
   align-self: center;
 }
 
-input, select {
+input,
+select {
   width: 100%;
   border: 1px solid #cdd7e5;
   border-radius: 12px;
-  padding: 12px 14px;
+  padding: 12px 16px; /* chỉnh để input bằng select */
   font-size: 15px;
   background: #fff;
+  box-sizing: border-box;
 }
 
 .actions {
