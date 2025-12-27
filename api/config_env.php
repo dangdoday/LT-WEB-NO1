32d0895ea955638d8f272993567073dd9bee6b19
Nguyen Tuan Anh <anhtt33t@gmail.com>
2025-12-27 17:01:55 +0700

    Xoa file database khoi git va them gitignore

---
+++ api/config_env.php
@@ -0,0 +1,7 @@
+<?php
+// file: backend/config_env.php
+return [
+    'RECAPTCHA_SITE_KEY' => '6LcWiCssAAAAABaebeS39OCuLJK0ikMm4zC5RQkN',
+    'RECAPTCHA_SECRET_KEY' => '6LcWiCssAAAAAKDhkm7mquNJ0wypqdV2Ikicr-ZJ'
+];
+?>
